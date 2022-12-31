ce62f8b8576ef6ae3cfe3836781d136ea9b3be88
Vincil Lau <vincillau@outlook.com>
2022-12-31 18:13:59 +0800

    feat: 添加core.hpp

---
+++ include/coro/core.hpp
@@ -0,0 +1,42 @@
+#ifndef CORO_INCLUDE_CORO_CORE_HPP_
+#define CORO_INCLUDE_CORO_CORE_HPP_
+
+#include <type_traits>
+
+#include "exception.hpp"
+#include "promise.hpp"
+#include "sched/sched.hpp"
+
+namespace coro {
+
+template <typename T, typename Func>
+void runFunc(const Func& func, const Promise<T>& promise) {
+  promise.resolve(func());
+}
+
+template <typename Func>
+void runFunc(const Func& func, const Promise<void>& promise) {
+  func();
+  promise.resolve();
+}
+
+template <typename Func>
+Promise<typename std::result_of<Func()>::type> spawn(Func func) {
+  Promise<typename std::result_of<Func()>::type> promise;
+  auto coro_func = [func, promise]() {
+    try {
+      runFunc(func, promise);
+    } catch (const Exception& e) {
+      promise.reject(e.error());
+    }
+  };
+  auto coro = std::make_shared<sched::Coro>(std::move(coro_func), 1024 * 64);
+  sched::Sched::add(coro);
+  return promise;
+}
+
+inline static void yield() { sched::Sched::yield(); }
+
+}  // namespace coro
+
+#endif  // CORO_INCLUDE_CORO_CORE_HPP_
